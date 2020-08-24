640c3fb0e4e32feacb3b28343a5b42434c8cad87
Hirito <>
2020-08-24 20:29:40 +0800

    create github webhook handler

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
+const github = require('github-webhook-handler')({
+    path: '/webhook',
+    secret: process.env.SECRET || '',
+    port: port
+});
 
 app.get('/', (req, res) => {
     res.send('hello');
 });
+
 app.listen(port, () => console.log(`listening on http://localhost:${port}`));
+
+github.on('error', err => console.error('Error:', err.message));
+
+github.on('push', ({ payload }) => console.log(`Receive $push for ${payload.repository.name} to ${payload.repository.ref}`));
