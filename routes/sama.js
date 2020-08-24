7fffec87a95c7aa15ef867737f77adb0aa2db914
Hirito <>
2020-08-25 00:16:22 +0800

    test webhook

--- routes/sama.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-router.post('/', (req, res) => {
-    console.log(`REF: ${req.body.payload.ref}\nName: ${req.body.payload.repository.name}`);
-    res.json('Yes,Sir');
-})
-
-module.exports = router;
