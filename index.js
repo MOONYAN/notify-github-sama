30028c418f45e0bbe5b1bd0d4fd2a919df722bfb
Hirito <>
2020-08-24 22:45:34 +0800

    test webhook

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const app = require('express')();
 const bodyParser = require('body-parser');
 const port = process.env.PORT || 3000;
-const handler = require('github-webhook-handler')({ path: '/webhook', secret: '' });
+// const handler = require('github-webhook-handler')({ path: '/webhook', secret: '' });
 
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
@@ -11,26 +11,10 @@ app.get('/', (req, res) => {
 })
 
 app.post('/webhook', (req, res) => {
-    console.log(req.body)
+    console.log('Received a push event for %s to %s',
+        req.body.payload.repository.name,
+        req.body.payload.ref);
     res.json('Yes,Sir');
 });
 
 app.listen(port, () => console.log(`Listen on http://localhost:${port}`));
-
-handler.on('error', function (err) {
-    console.error('Error:', err.message)
-})
-
-handler.on('push', function (event) {
-    console.log('Received a push event for %s to %s',
-        event.payload.repository.name,
-        event.payload.ref)
-})
-
-handler.on('issues', function (event) {
-    console.log('Received an issue event for %s action=%s: #%d %s',
-        event.payload.repository.name,
-        event.payload.action,
-        event.payload.issue.number,
-        event.payload.issue.title)
-})
